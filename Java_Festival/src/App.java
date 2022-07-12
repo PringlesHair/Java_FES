b5f3fa03d3853ae0b8ab3fa2ca269bfa61ce0d94
Scarlett0JS <scarlett.js1010@gmail.com>
2022-07-12 13:47:15 +0900

    0090

--- Java_Festival/src/App.java
+++ Java_Festival/src/App.java
@@ -1,23 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Hello");
+        System.out.println("World!");
+        
         
     }
 }
