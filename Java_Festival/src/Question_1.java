e586f90a4725f354e99b669e4bf6d52b8575a799
PringlesHair <86374398+PringlesHair@users.noreply.github.com>
2022-07-12 13:49:18 +0900

    mytest

---
+++ Java_Festival/src/Question_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("정수 입력 : ");
+        int num = sc.nextInt();
+
+        result(num);
+        // use_math(num);
+
+        sc.close();
+    }
+
+    private static void result(int num) {
+        System.out.println("결과 : " + (num / 100) * 100);
+    }
+
+    private static void use_math(int num) {
+        // Math 사용
+        // http://www.tcpschool.com/java/java_api_math
+        System.out.println("결과 : " + (int) Math.floor(num * 0.01) * 100);
+    }
+}
