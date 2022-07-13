eaa730c5e056a3dca99eb7e8eb14a17be83f0228
PringlesHair <86374398+PringlesHair@users.noreply.github.com>
2022-07-13 14:15:46 +0900

    가우스합을 이용한 풀이

--- Java_Festival/src/CKjung/Question_18.java
+++ Java_Festival/src/CKjung/Question_18.java
@@ -6,13 +6,23 @@ public class Question_18 {
         Scanner sc = new Scanner(System.in);
         System.out.print("숫자를 입력하시오 : ");
         int num = sc.nextInt();
-        
-        int answer = 0;
-        for (int i = 1; i <= num; i++) {
-            answer+=i;
-        }
-        System.out.println("1부터 " + num + "까지의 합은 " + answer + "입니다.");
+
+        use_loop(num);
+        use_cal(num);
 
         sc.close();
+    }
+
+    private static void use_cal(int num) {
+        int answer = num * (num+1) / 2;
+        System.out.println("1부터 " + num + "까지의 합은 " + answer + "입니다.");
+    }
+
+    private static void use_loop(int num) {
+        int answer = 0;
+        for (int i = 1; i <= num; i++) {
+            answer += i;
+        }
+        System.out.println("1부터 " + num + "까지의 합은 " + answer + "입니다.");
     }
 }
